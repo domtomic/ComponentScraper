1ddadcf1d6b34f9c3c5d2f5f54c72157fac6fd4e
Domagoj Tomic <domagoj.tomic9@gmail.com>
2023-09-21 11:55:29 +0200

    elmatis scraper finds all components except the first subpage

--- scraper.py
+++ scraper.py
@@ -3,7 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-page_target = "Tevetron"
+page_target = "Elmatis"
 page_category = "IC"
 
 componentsDict = {
@@ -32,19 +32,20 @@ if (page_target == "Tevetron"):
         print("ERROR: Tevetron doesn't have the requested category!")
         sys.exit(2)
 elif (page_target == "Elmatis"):
+    URL_base = "http://www.elmatis.hr/Articles.aspx"
     if (page_category == "IC"):
-        URL_base = "http://www.elmatis.hr/Products.aspx?categoryId=81"
+        elmatis_category = "I. C. KRUGOVI I PODNOŽJA"
     elif (page_category == "transistors"):
-        URL_base = "http://www.elmatis.hr/Products.aspx?categoryId=159"
+        elmatis_category = "TRANZISTORI, LISKUNI, MODULI"
     else:
         print("ERROR: Elmatis doesn't have the requested category!")
         sys.exit(3)
 
 page = requests.get(URL_base)
 soup = BeautifulSoup(page.content, "html.parser")
-results = soup.find_all("div", class_="product-inner")
 
 if (page_target == "Tevetron"):
+    results = soup.find_all("div", class_="product-inner")
     i = 0
     while results:
         for result in results:
@@ -61,7 +62,63 @@ if (page_target == "Tevetron"):
         soup = BeautifulSoup(page.content, "html.parser")
         results = soup.find_all("div", class_="product-inner")
 elif (page_target == "Elmatis"):
-    print("Not implemented")
-    sys.exit(4)
+    ## find all links for a given category subpages
+    category_list = soup.find("div", id="navigationContainer").find_all("a")
+    category_links = []
+    N = len(category_list)
+    found_subpage = False
+    for i_cat in range(N):
+        subpage_addr = category_list[i_cat].get("href")
+        if (subpage_addr == "#"):
+            if (found_subpage):
+                break
+            subpage_name = category_list[i_cat].text.strip()
+            if (subpage_name == elmatis_category):
+                found_subpage = True
+        elif (found_subpage):
+            category_links.append(subpage_addr)
+    if (len(category_links) == 0):
+        print("No such category")
+        sys.exit(4)
+    ## go to every subpage for a given category
+    URL_base_2 = "http://www.elmatis.hr"
+    for subpage_link in category_links:
+        URL_subpage = URL_base_2 + subpage_link
+        elmatis_subpage = requests.get(URL_subpage)
+        print(URL_subpage)
+        req_page = 1
+        while True:
+            print(req_page)
+            req_page += 1
+            elmatis_soup = BeautifulSoup(elmatis_subpage.content, "html.parser")
+            if(elmatis_subpage.status_code != 200):
+                break
+            VIEWSTATEvalue  = elmatis_soup.find("input", id="__VIEWSTATE").get("value")
+            VIEWSTATEGENERATORvalue  = elmatis_soup.find("input", id="__VIEWSTATEGENERATOR").get("value")
+            EVENTVALIDATIONvalue  = elmatis_soup.find("input", id="__EVENTVALIDATION").get("value")
+            results = elmatis_soup.find("table", class_="list")
+            component_PN = []
+            component_desc = []
+            component_price = []
+            component_stock = []
+            rows = results.find_all("tr")
+            Nrows = len(rows)
+            ## last two rows are for page numbers
+            for i in range(Nrows-2):
+                row = rows[i]
+                cells = row.find_all("td")
+                if (len(cells) > 0):
+                    component_PN.append(cells[0].text.strip())
+                    component_desc.append(cells[1].text.strip())
+                    component_price.append(cells[2].find("div", class_="price-kn").text.strip())
+                    component_stock.append(cells[3].text.strip())
+            data = {
+                "__EVENTTARGET" : "ctl00$Main$ucProductList$grdProductListPublic",
+                "__EVENTARGUMENT" : "Page${}".format(req_page+1),
+                "__VIEWSTATEGENERATOR" : VIEWSTATEGENERATORvalue,
+                "__VIEWSTATE" : VIEWSTATEvalue,
+                "__EVENTVALIDATION" : EVENTVALIDATIONvalue
+            }
+            elmatis_subpage = requests.post(URL_subpage, data)
 
 f.close()
